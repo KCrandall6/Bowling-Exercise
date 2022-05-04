93a40633e29e67f273cd8e314e532e2b2634ada6
KCrandall6 <kcrandall67@gmail.com>
2022-05-03 17:10:54 -0700

    button logic added, bug fix for double loading build module and babel polyfill

--- client/Board.jsx
+++ client/Board.jsx
@@ -1,23 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import '../style.scss';
 
 const Board = (props) => {
 
   // props data
   const [game, setGame] = props.functions;
+  const [pins, setPins] = useState(0);
+  const [bowlCount, setBowlCount] = useState(1);
 
-  let renderScores = (frame, roll) => {
-    const {frames} = this.props
-    return frames[frame] ? frames[frame][roll] : ''
+
+  // maybe make pins an array, push into array; for first element add to 0 for first score
+  const pinCount = (num) => {
+    setPins(pins + num);
+    setBowlCount(bowlCount + 1);
+    if (bowlCount >= 2) {
+      setBowlCount(1);
+      setPins(0);
+    }
   }
+  
+  console.log('bowl', bowlCount);
+  console.log('pins', pins);
 
   return (
     <div>
       <p>scoreboard</p>
       <div className='Container'>
         <table id='table' className='Scorecard' cellPadding='1' cellSpacing='0'>
-          <tbody>
-            <tr>
+          <thead>
+            <tr id='frame-name'>
               <th id='c1' colSpan='6'>Frame 1</th>
               <th id='c2' colSpan='6'>Frame 2</th>
               <th id='c3' colSpan='6'>Frame 3</th>
@@ -30,34 +41,27 @@ const Board = (props) => {
               <th id='c10' colSpan='6'>Frame 10</th>
               <th id='c11' colSpan='6'>Total Score</th>
             </tr>
-            {/* <tr>
-              <td id='r1' colSpan='3'>{this.renderScores(0,0)}</td><td id='r2' colSpan='3'>{this.renderScores(0,1)}</td>
-              <td id='r3' colSpan='3'>{this.renderScores(1,0)}</td><td id='r4' colSpan='3'>{this.renderScores(1,1)}</td>
-              <td id='r5' colSpan='3'>{this.renderScores(2,0)}</td><td id='r6' colSpan='3'>{this.renderScores(2,1)}</td>
-              <td id='r7' colSpan='3'>{this.renderScores(3,0)}</td><td id='r8' colSpan='3'>{this.renderScores(3,1)}</td>
-              <td id='r9' colSpan='3'>{this.renderScores(4,0)}</td><td id='r10' colSpan='3'>{this.renderScores(4,1)}</td>
-              <td id='r11' colSpan='3'>{this.renderScores(5,0)}</td><td id='r12' colSpan='3'>{this.renderScores(5,1)}</td>
-              <td id='r13' colSpan='3'>{this.renderScores(6,0)}</td><td id='r14' colSpan='3'>{this.renderScores(6,1)}</td>
-              <td id='r15' colSpan='3'>{this.renderScores(7,0)}</td><td id='r16' colSpan='3'>{this.renderScores(7,1)}</td>
-              <td id='r17' colSpan='3'>{this.renderScores(8,0)}</td><td id='r18' colSpan='3'>{this.renderScores(8,1)}</td>
-              <td id='r19' colSpan='2'>{this.renderScores(9,0)}</td><td id='r20' colSpan='2'>{this.renderScores(9,1)}</td><td id='r21' colSpan='2'>{this.renderScores(9,2)}</td>
-              <td id='total-score' className='Total' colSpan='6'>{totalScore}</td>
+        </thead>
+          <tbody>
+            <tr id='bowl-scores'>
             </tr>
-            <tr>
-              <td id='cumulative-score-f1' colSpan='6'>{game.cumulativeScores[0]}</td>
-              <td id='cumulative-score-f2' colSpan='6'>{game.cumulativeScores[1]}</td>
-              <td id='cumulative-score-f3' colSpan='6'>{game.cumulativeScores[2]}</td>
-              <td id='cumulative-score-f4' colSpan='6'>{game.cumulativeScores[3]}</td>
-              <td id='cumulative-score-f5' colSpan='6'>{game.cumulativeScores[4]}</td>
-              <td id='cumulative-score-f6' colSpan='6'>{game.cumulativeScores[5]}</td>
-              <td id='cumulative-score-f7' colSpan='6'>{game.cumulativeScores[6]}</td>
-              <td id='cumulative-score-f8' colSpan='6'>{game.cumulativeScores[7]}</td>
-              <td id='cumulative-score-f9' colSpan='6'>{game.cumulativeScores[8]}</td>
-              <td id='cumulative-score-f10' colSpan='6'>{game.cumulativeScores[9]}</td>
-              <td id='total-score' colSpan='6'></td>
-            </tr> */}
+            <tr id='total-scores'>
+            </tr>
           </tbody>
         </table>
+      </div>
+      <div className='pin-buttons'>
+        <button type="button" onClick={() => pinCount(0)}>0</button>
+        <button hidden={pins > 9} type="button" onClick={() => pinCount(1)}>1</button>
+        <button hidden={pins > 8} type="button" onClick={() => pinCount(2)}>2</button>
+        <button hidden={pins > 7} type="button" onClick={() => pinCount(3)}>3</button>
+        <button hidden={pins > 6} type="button" onClick={() => pinCount(4)}>4</button>
+        <button hidden={pins > 5} type="button" onClick={() => pinCount(5)}>5</button>
+        <button hidden={pins > 4} type="button" onClick={() => pinCount(6)}>6</button>
+        <button hidden={pins > 3} type="button" onClick={() => pinCount(7)}>7</button>
+        <button hidden={pins > 2} type="button" onClick={() => pinCount(8)}>8</button>
+        <button hidden={pins > 1} type="button" onClick={() => pinCount(9)}>9</button>
+        <button hidden={pins > 0} type="button" onClick={() => pinCount(10)}>10</button>
       </div>
     </div>
   )
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,58 @@
-const path = require('path');
-const process = require('process');
-require('dotenv').config();
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path');
+
+
 module.exports = {
-  mode: process.env.NODE_ENV || 'production',
-  devServer: {
-    host: 'localhost',
-    port: 8080,
-    proxy: {
-      '/': 'http://localhost:3000',
+    mode: process.env.NODE_ENV,
+    entry: ['babel-polyfill', './client/index.js'],
+    output: {
+        path: path.resolve('build'),
+        filename: 'bundle.js',
     },
-  },
-  entry: path.resolve(__dirname, 'client/index.js'),
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: 'index.html' }),
-    new MiniCssExtractPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.[tj]sx?$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        },
-      },
-      {
-        test: /\.s?[ac]ss$/i,
-        exclude: /node_modules/,
-        use: [
-          process.env.NODE_ENV === 'production'
-            ? MiniCssExtractPlugin.loader
-            : 'style-loader',
-          'css-loader',
-          'sass-loader',
+    plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin({template: path.join(__dirname, './index.html')})],
+    module: {
+        rules: [
+            {
+                test: /\.(jsx?)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                  },
+                },
+              },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use:[{
+                        loader: 'file-loader',
+                }]
+            },
+            {
+                test: /\.s?[ac]ss$/i,
+                exclude: /node_modules/,
+                use: [
+                  process.env.NODE_ENV === 'production'
+                    ? MiniCssExtractPlugin.loader
+                    : 'style-loader',
+                  'css-loader',
+                  'sass-loader',
+                ],
+              },
         ],
+    },
+    devServer: {
+      compress: true,
+      port: 8080,
+      hot: true,
+      proxy: {
+        '/api': 'http://localhost:3000',
+        secure: true,
       },
-    ],
-  },
-};
+      static: {
+        directory: path.join(__dirname, '/'),
+        publicPath: '/',
+      },
+    }
+}
