062f205c0e17c4b3c9201a96ae9f1caa47ac0c1d
KCrandall6 <kcrandall67@gmail.com>
2022-05-06 02:44:03 -0700

    migrated board to scoreCalculator component with new class; finished exercise

--- client/App.jsx
+++ client/App.jsx
@@ -1,24 +1,97 @@
-import React, { Component, useState } from 'react';
+
+import React, { Component, useState } from "react";
 import '../style.scss';
-import Board from './Board.jsx'
+import ScoreCalculator from './ScoreCalculator.jsx';
+
 
 const App = () => {
 
-  const [game, setGame] = useState({
-      frames: '',
-      cumulativeScores: [],
-      pins: '',
-      rolls: '',
-  })
+  const [rollCount, setRollCount] = useState(0);
 
-  // const [total, setTotal] = useState(cumulativeScores.slice(-1)[0])
-
-  return (
-    <div>
-      <h1>Hello Bowlers!</h1>
-      <div>
-        <Board game = {game} functions={[game, setGame]}/>
+  const Frame = ({ frameNumber, frame1, frame2, cumulative, score }) => (
+    <div className="frame">
+      <div className="frame-number">Frame {frameNumber}</div>
+      <div className="frame-score">
+        <div className="frame1">{frame1}</div>
+        <div className="frame2">{frame2}</div>
+        <div className="cumulative">{cumulative}</div>
       </div>
+      <div className="running-score">{!isNaN(score) && score}</div>
+    </div>
+  );
+
+  const incrementer = (i) => {
+    handleRoll(i);
+    setRollCount(rollCount + 1);
+  }
+
+  const Controls = ({ handleRoll, pinsUp }) => (
+    <div className="controls">
+      <div className="controls-left">
+        {[...Array(pinsUp + 1)].map((o, i) => (
+          <button key={i} className="roll" onClick={() => handleRoll(i)}>
+            {i}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+
+  class ScoreBoard extends Component {
+    constructor() {
+      super();
+      this.game = ScoreCalculator.create();
+      this.state = {
+        score: this.game.score()
+      };
+    }
+
+    roll = pins => {
+      this.game.roll(pins);
+      this.setState({ score: this.game.score() });
+    };
+
+    pinsUp = () => this.game.pinsUp();
+
+    render() {
+      const { score } = this.state;
+
+      return (
+        <div>
+
+          <div className="player-name">Enter Name:
+            <input></input>
+          </div>
+
+          <div className="score-board">
+            {[...Array(10)].map((o, i) => (
+              <Frame
+                key={i}
+                frameNumber={i + 1}
+                frame1={score[i].frame1}
+                frame2={score[i].frame2}
+                cumulative={score[i].cumulative}
+                score={score[i].cumulativeScore}
+              />
+            ))}
+          </div>
+          <div className='button-area'>
+            <h3>How many pins did you hit</h3>
+          </div>
+          <Controls handleRoll={this.roll} setRollCount={setRollCount} pinsUp={this.pinsUp()} />
+        </div>
+      );
+    }
+  }
+
+
+  
+  return (
+    <div className="App">
+      <div className='title'>
+        <h1>- Hello Bowlers! -</h1>
+      </div>
+      <ScoreBoard />
     </div>
   )
 }
